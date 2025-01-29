e67520977295ab5a98087ffd3f98ef51aa673974
Shree Ravi Adhikari <shreeraviadhikari@gmail.com>
2025-01-29 10:58:59 -0600

    feat: add logo

--- src/commons/Navbar.js
+++ src/commons/Navbar.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-600 p-4">
+    <nav className="bg-gray-600 ">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-white text-2xl font-bold">
-          My Portfolio
+          <Image src="/logo-white.png" alt="logo" height={200} width={200} />
         </Link>
         <div className="space-x-4">
           <Link href="/about" className="text-white hover:text-gray-200">

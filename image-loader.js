d7139cc2738fcfd73456154eaafd20b8cf8f9d2e
Shree Ravi Adhikari <shreeraviadhikari@gmail.com>
2025-01-30 17:25:06 -0600

    fix: use github images for loader

--- image-loader.js
+++ image-loader.js
@@ -1,3 +1,9 @@
-export default function imageLoader({ src }) {
+function githubLoader({ src }) {
+  return `https://raw.githubusercontent.com/shreeraviadhikari/shreeraviadhikari.com/refs/heads/master/public/${src}`;
+}
+
+function localLoader({ src }) {
   return `http://localhost:8000${src}`;
 }
+
+export default githubLoader;
